7d66960916cd99467ec99493962f5f802419a8d6
Jayrane03 <jayu.rane32003@gmail.com>
2023-02-04 20:29:39 +0530

    Responsivenes

--- index.js
+++ index.js
@@ -13,3 +13,18 @@
       navlist.classList.remove("sticky");
     }
   }
+
+
+  const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId)
+    if (toggle && nav) {
+      toggle.addEventListener('click', () => {
+        nav.classList.toggle('show')
+      })
+    }
+  
+  
+  }
+  showMenu('nav-toogle', 'nav-menu')
+  
